27394c8b7871246ea533a50811a5e522b2a593df
SangWookPark <wkahdla12346@gmail.com>
2020-01-20 18:16:55 +0900

    rxMemo: memo share

--- Project/RxMemo/RxMemo/VC/MemoDetailViewController.swift
+++ Project/RxMemo/RxMemo/VC/MemoDetailViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class MemoDetailViewController: UIViewController, ViewModelBindableType {
    
@@ -45,5 +47,16 @@ class MemoDetailViewController: UIViewController, ViewModelBindableType {
       .disposed(by: rx.disposeBag)
       
       editButton.rx.action = viewModel.makeEditAction()
+      
+      shareButton.rx.tap
+         .throttle(.milliseconds(500), scheduler: MainScheduler.instance)
+         .subscribe(onNext: { [weak self] in
+            guard let memo = self?.viewModel.memo.content else { return }
+            
+            let vc = UIActivityViewController(activityItems: [memo], applicationActivities: nil)
+            self?.present(vc, animated: true, completion: nil)
+         })
+         .disposed(by: rx.disposeBag)
    }
+   
 }
