eccedca0d1578fcd06b6042356d9eebc8a6a6910
SangWookPark <wkahdla12346@gmail.com>
2020-03-03 11:53:15 +0900

    Compose TextView Keyboard Inset

--- Project/RxMemo/RxMemo/VC/MemoComposeViewController.swift
+++ Project/RxMemo/RxMemo/VC/MemoComposeViewController.swift
@@ -39,5 +39,30 @@ class MemoComposeViewController: UIViewController, ViewModelBindableType {
          .withLatestFrom(contentTextView.rx.text.orEmpty)
          .bind(to: viewModel.saveAction.inputs)
          .disposed(by: rx.disposeBag)
+      
+      let willShowOberservable = NotificationCenter.default.rx.notification(UIResponder.keyboardWillShowNotification)
+         .map { ($0.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.height ?? 0 }
+      
+      let willHideObservable = NotificationCenter.default.rx.notification(UIResponder.keyboardWillHideNotification)
+         .map { noti -> CGFloat in 0 }
+      
+      let keyboardObservable = Observable.merge(willShowOberservable, willHideObservable).share()
+      
+      keyboardObservable
+         .subscribe(onNext: { [weak self] height in
+            guard let strongSelf = self else { return }
+            
+            var inset = strongSelf.contentTextView.contentInset
+            inset.bottom = height
+            
+            var scrollInset = strongSelf.contentTextView.scrollIndicatorInsets
+            scrollInset.bottom = height
+            
+            UIView.animate(withDuration: 0.3) {
+               strongSelf.contentTextView.contentInset = inset
+               strongSelf.contentTextView.scrollIndicatorInsets = scrollInset
+            }
+         })
+         .disposed(by: rx.disposeBag)
    }
 }
