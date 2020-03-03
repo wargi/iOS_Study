12aaaf246f6cd86c5801f9d084a303b6e771d8b0
SangWookPark <wkahdla12346@gmail.com>
2020-03-03 17:18:40 +0900

    RxWeather: DelegateProxy

--- Project/RxWeather/RxWeather/Service/RxCLLocationManagerDelegateProxy.swift
+++ Project/RxWeather/RxWeather/Service/RxCLLocationManagerDelegateProxy.swift
@@ -24,3 +24,37 @@ import UIKit
 import CoreLocation
 import RxSwift
 import RxCocoa
+
+extension CLLocationManager: HasDelegate {
+   public typealias Delegate = CLLocationManagerDelegate
+}
+
+public class RxCLLocationManagerDelegateProxy: DelegateProxy<CLLocationManager, CLLocationManagerDelegate>, DelegateProxyType, CLLocationManagerDelegate {
+   public init(locationManager: CLLocationManager) {
+      super.init(parentObject: locationManager, delegateProxy: RxCLLocationManagerDelegateProxy.self)
+   }
+   
+   public static func registerKnownImplementations() {
+      self.register { RxCLLocationManagerDelegateProxy(locationManager: $0) }
+   }
+}
+
+extension Reactive where Base: CLLocationManager {
+   var delegate: DelegateProxy<CLLocationManager, CLLocationManagerDelegate> {
+      return RxCLLocationManagerDelegateProxy.proxy(for: base)
+   }
+   
+   var didUpdateLocations: Observable<[CLLocation]> {
+      return delegate.methodInvoked(#selector(CLLocationManagerDelegate.locationManager(_:didUpdateLocations:)))
+         .map { parameters in
+            return parameters[1] as! [CLLocation]
+         }
+   }
+   
+   var didChangeAuthorizationStatus: Observable<CLAuthorizationStatus> {
+      return delegate.methodInvoked(#selector(CLLocationManagerDelegate.locationManager(_:didChangeAuthorization:)))
+         .map { parameters in
+            return CLAuthorizationStatus(rawValue: parameters[1] as! Int32) ?? CLAuthorizationStatus.notDetermined
+      }
+   }
+}
