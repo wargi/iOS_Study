24fb1b31210dce56fdd27c183f9cec03725d3573
SangWookPark <wkahdla12346@gmail.com>
2020-03-03 16:43:19 +0900

    RxWeather: MainViewModel

--- Project/RxWeather/RxWeather/ViewModel/MainViewModel.swift
+++ Project/RxWeather/RxWeather/ViewModel/MainViewModel.swift
@@ -1,25 +1,3 @@
-//
-//  Copyright (c) 2019 KxCoding <kky0317@gmail.com>
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
-//
-
 import Foundation
 import RxSwift
 import RxCocoa
@@ -29,6 +7,9 @@ import NSObject_Rx
 typealias SectionModel = AnimatableSectionModel<Int, WeatherData>
 
 class MainViewModel: HasDisposeBag {
+   
+   
+   
    static let tempFormatter: NumberFormatter = {
       let formatter = NumberFormatter()
       formatter.minimumFractionDigits = 0
@@ -43,5 +24,57 @@ class MainViewModel: HasDisposeBag {
       return formatter
    }()
    
+   let title: BehaviorRelay<String>
+   let sceneCoordinator: SceneCoordinatorType
+   let weatherApi: WeatherApiType
+   let locationProvider: LocationProviderType
    
+   internal init(title: String, sceneCoordinator: SceneCoordinatorType, weatherApi: WeatherApiType, locationProvider: LocationProviderType) {
+      self.title = BehaviorRelay(value: title)
+      self.sceneCoordinator = sceneCoordinator
+      self.weatherApi = weatherApi
+      self.locationProvider = locationProvider
+      
+      locationProvider.currentAddress().bind(to: self.title)
+         .disposed(by: disposeBag)
+   }
+   
+   var weatherData: Driver<[SectionModel]> {
+      return locationProvider.currentLocation()
+         .flatMap { [unowned self] in
+            self.weatherApi.fetch(location: $0)
+               .asDriver(onErrorJustReturn: (nil, []))
+      }
+         .map { (summary, forecast) in
+            var summaryList = [WeatherData]()
+            if let summary = summary as? WeatherData {
+               summaryList.append(summary)
+            }
+            
+            return [
+               SectionModel(model: 0, items: summaryList),
+               SectionModel(model: 1, items: forecast as! [WeatherData])
+            ]
+         }
+         .asDriver(onErrorJustReturn: [])
+   }
+   
+   let dataSource: RxTableViewSectionedAnimatedDataSource<SectionModel> = {
+      let ds = RxTableViewSectionedAnimatedDataSource<SectionModel>(configureCell: { (dataSource, tableView, indexPath, data) -> UITableViewCell in
+         switch indexPath.section {
+         case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: SummaryTableViewCell.identifier, for: indexPath) as! SummaryTableViewCell
+            cell.configure(from: data, tempFormatter: MainViewModel.tempFormatter)
+            return cell
+         case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: ForecastTableViewCell.identifier, for: indexPath) as! ForecastTableViewCell
+            cell.configure(from: data, dateFormatter: MainViewModel.dateFormatter, tempFormatter: MainViewModel.tempFormatter)
+            return cell
+         default:
+            fatalError()
+         }
+      })
+      
+      return ds
+   }()
 }
