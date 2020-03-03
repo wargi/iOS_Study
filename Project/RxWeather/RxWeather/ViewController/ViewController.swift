4aacee622f7a8dcbe097ada4e0f197c5d05bdc55
SangWookPark <wkahdla12346@gmail.com>
2020-03-03 16:48:17 +0900

    RxWeather: view model binding

--- Project/RxWeather/RxWeather/ViewController/ViewController.swift
+++ Project/RxWeather/RxWeather/ViewController/ViewController.swift
@@ -1,25 +1,3 @@
-//
-//  Copyright (c) 2019 KxCoding <kky0317@gmail.com>
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
-//
-
 import UIKit
 import CoreLocation
 import RxSwift
@@ -34,7 +12,13 @@ class ViewController: UIViewController, ViewModelBindableType {
    @IBOutlet weak var listTableView: UITableView!
    
    func bindViewModel() {
+      viewModel.title
+         .bind(to: locationLabel.rx.text)
+         .disposed(by: rx.disposeBag)
       
+      viewModel.weatherData
+         .drive(listTableView.rx.items(dataSource: viewModel.dataSource))
+         .disposed(by: rx.disposeBag)
    }
    
    override func viewDidLoad() {
