a342be356f20939ebe3350ec085477279fac777c
wargi <wkahdla12346@gmail.com>
2021-11-21 22:51:50 +0900

    +++Update: Vesion0.8.1

--- Practice/RoutePath/RoutePath/ViewController.swift
+++ Practice/RoutePath/RoutePath/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+import ASPM
+
 
 class ViewController: UIViewController {
+    lazy var tableView: UITableView = {
+        let v = UITableView()
+        return v
+    }()
+    
+    let subject = BehaviorRelay<Int>(value: 10)
+    
+    @objc public func f() {
+        
+    }
+    
+    static public var re: Int?
+    
     //경로선 테스트
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        
+        LogUtil.d(ASPM().text)
     }
-
-
 }
-
